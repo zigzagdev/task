e9523f3c436905a7c833f60ce3363564bd6c62f2
zigzagdev <msts.oo0131@gmail.com>
2023-11-01 22:25:12 +0900

    initial-commit

---
+++ app.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>TodoApp</h1>
+    <h2>タスク追加</h2>
+    <input v-model="taskName">
+    <button @click="addTask()">追加</button>
+    <h2>タスク名</h2>
+    <div v-for="taskName in taskNameList" :key="taskName">
+      {{ taskName }}
+      <button @click="completeTask(taskName)">完了</button>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+const taskNameList = ref<string[]>([]);
+const taskName = ref<string>('');
+const addTask = () => {
+  if (taskName.value === '') {
+    return;
+  }
+  taskNameList.value.push(taskName.value);
+  taskName.value = '';
+}
+const completeTask = (completedTaskName: string) => {
+  taskNameList.value = taskNameList.value.filter((taskName) => completedTaskName !== taskName);
+}
+</script>
